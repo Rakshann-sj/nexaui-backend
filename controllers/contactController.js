5778d6c4ea108b73b4f3f73ba1b025067ecf2cd7
Rakshan <rakshann16.sj@gmail.com>
2026-05-26 16:42:40 +0530

    nothing

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -12,7 +12,7 @@ exports.submitContact = async (req, res, next) => {
 
     // Save to DB
     await Message.create({ name, email, subject, message });
-await fetch('http://192.168.0.19:5678/webhook/nexaui-contact', {
+await fetch('http://host.docker.internal:5678/webhook/nexaui-contact', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
